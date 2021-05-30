0edcebb7e19a1933bed8c800720776c5db4751d7
ManasaBadeti <74001906+ManasaBadeti@users.noreply.github.com>
2021-05-30 21:10:26 +0530

    Update App.js

--- Part2/Ex2.1/App.js
+++ Part2/Ex2.1/App.js
@@ -1,6 +1,43 @@
-import Course from './components/Course'
-
 const App = () => {
+  const Header = (args) => {
+  
+    return (
+      <div >
+       <h1 style={{color: "blue", fontSize:40, fontFamily:"Times New Roman"}}>{args.course}</h1>
+      </div>
+    )
+  }
+  
+  const Content =  ({parts}) => {
+    const [part1, part2, part3, part4] = parts;
+    return (
+      <div style={{fontSize:30,color:"hotpink",fontStyle:"italic",fontFamily:"serif"}}>
+        <p>  {part1.name}     {part1.exercises} </p>
+        <p> {part2.name}      {part2.exercises} </p>
+        <p> {part3.name}       {part3.exercises} </p>
+        <p> {part4.name}       {part4.exercises} </p>
+      </div>
+    )
+  }
+  
+
+  
+  
+  const Course = ({course}) => {
+    
+    return (
+      
+      <div align="center"  >
+       <Header course={course.name} />
+       <Content parts={course.parts}  />
+      
+      
+      
+     
+      </div>
+      
+    )
+  }
   const course = {
     name: 'Half Stack application development',
     id: 1,
@@ -20,6 +57,7 @@ const App = () => {
         exercises: 14,
         id: 3
       }
+      
     ]
   }
 
